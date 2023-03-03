aada7d5487d0c403739a7165fb8c2498978f7f67
allexmorozovv <allexmorozovv@gmail.com>
2023-03-03 18:37:04 +0300

    add get Posts from server

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,13 +1,25 @@
+"use client"
 import styles from "@/app/page.module.css";
+import {useEffect, useState} from "react";
+import {getAllPosts, Post} from "@/app/services/post";
+
+// interface PropsType{
+//     posts:Post[]
+// }
 
 
+export default function Main() {
+const [posts,setPosts]=useState<Post[]>([])
 
+    useEffect(()=>{
+        getAllPosts()
+            .then((data)=>setPosts(data))
+    },[])
 
-
-export default function Posts() {
     return (
             <div className={styles.main}>
-                <h1 >Список постов</h1>
+                <h1 ></h1>
+                <div>{posts.map((item)=><div key={item.id}>{item.title}</div>)}</div>
             </div>
     )
 }
