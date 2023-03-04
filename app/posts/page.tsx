6b3b045b3279f38dcb098bb396af721df95bca1b
allexmorozovv <allexmorozovv@gmail.com>
2023-03-04 17:54:06 +0300

    add input with search

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,23 +3,32 @@ import styles from "@/app/page.module.css";
 import {useEffect, useState} from "react";
 import {getAllPosts, Post} from "@/app/services/post";
 
-// interface PropsType{
-//     posts:Post[]
+// interface PropsType {
+//     posts: Post[]
 // }
 
 
 export default function Main() {
-const [posts,setPosts]=useState<Post[]>([])
+    const [posts, setPosts] = useState<Post[]>([])
+    const [filterData, setFilterData] = useState<string>('')
 
-    useEffect(()=>{
+    const changeFilterData = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterData(event.target.value)
+    }
+
+    useEffect(() => {
         getAllPosts()
-            .then((data)=>setPosts(data))
-    },[])
+            .then((data) => setPosts(data))
+    }, [])
 
     return (
-            <div className={styles.main}>
-                <h1 ></h1>
-                <div>{posts.map((item)=><div key={item.id}>{item.title}</div>)}</div>
-            </div>
+        <div className={styles.main}>
+            <h1></h1>
+            <input placeholder={'search'} value={filterData} onChange={changeFilterData}/>
+            <div>{posts
+                .filter(({title}) => title.includes(filterData))
+                .map((item) => <div
+                key={item.id}>{item.title}</div>)}</div>
+        </div>
     )
 }
